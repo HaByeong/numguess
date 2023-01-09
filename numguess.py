3360878bafda3853f703d9ec0c7e8b89eff1a8cd
HaByeong <jhschris8080@naver.com>
2023-01-09 16:39:59 +0900

    feat: numguess

---
+++ numguess.py
@@ -0,0 +1,16 @@
+from random import randint
+
+#Make randint
+randint(1,100)
+#Make answer
+answer=randint(1,100)
+#Print answer
+print(answer)
+#Have user_name
+user_name=input("write your name")
+#Hello! user!
+print("Hello! "+user_name)
+#Think your user_num(1~100)
+user_num=input("What's is your num?")
+#Print user_num
+print(user_num)

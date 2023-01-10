381bf3f24726041832e220fe5c565aa48f7729f4
HaByeong <jhschris8080@naver.com>
2023-01-10 15:12:14 +0900

    feat: Add Comparing user's guess with answer

--- numguess.py
+++ numguess.py
@@ -1,16 +1,26 @@
 from random import randint
-
-#Make randint
-randint(1,100)
+from time import sleep
 #Make answer
 answer=randint(1,100)
-#Print answer
-print(answer)
-#Have user_name
-user_name=input("write your name")
-#Hello! user!
-print("Hello! "+user_name)
 #Think your user_num(1~100)
-user_num=input("What's is your num?")
-#Print user_num
-print(user_num)
+user_name=input("What's is your name?")
+
+print(f"Hi!,{user_name}! Please be my guest!!")
+#Get and print User's guess
+guess = int(input(f"So {user_name}, Guess the number(1~100): "))
+#Compare answer with user's guess
+if guess == answer:
+    print('*****************')
+    sleep(1)
+    print('*****************')
+    sleep(1)
+    print('*****************')
+    sleep(1)
+    print(f"You got it right!!!! The answer is {answer}!!")
+elif(answer>guess):
+  print(f"That's too small..{user_name}")
+  print(f"You lose~! So sad...answer is~~{answer}!")
+elif(answer<guess):
+  print(f"That's too big..{user_name}")
+  print(f"You lose~! So sad...answer is~~{answer}!")
+

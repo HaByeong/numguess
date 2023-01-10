36fa5f78631a476405194cf6941cf4cefd3975e1
HaByeong <jhschris8080@naver.com>
2023-01-10 09:41:12 +0900

    feat: Add data in containers(test)

--- hello.py
+++ hello.py
@@ -3,9 +3,10 @@ username='김하냥'
 print('Hello',username)
 # Print greeting with username
 print('Hello',username)
-animals=['cat','dog']
+animals=['cat','dog','dragon']
 #DO(Trailing comma)
 menus=[
   'chicken',
   'ramen',
+  'mandu'
 ]

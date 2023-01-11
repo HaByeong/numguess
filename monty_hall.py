eefb582fcfa90dd3554145d1c7f826bb1c8374f6
HaByeong <jhschris8080@naver.com>
2023-01-11 11:00:21 +0900

    feat: Increase result Depend on user_choice

--- monty_hall.py
+++ monty_hall.py
@@ -1,8 +1,13 @@
 from random import shuffle, choice
+result={
+    'stay_to_win': 0,
+    'move_to_win': 0,
+}
 door=[0,0,1]#:goat, 1:sports car
 shuffle(door)
 user_choice=choice(door)
-print(user_choice)
-
-
-)
+if user_chioce==0:
+    result['move_to_win']+=1
+else:
+    result['stay_to_win']+=1
+print(result)

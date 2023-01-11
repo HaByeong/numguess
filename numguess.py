01ac13b4212e10d4c82563741010c5e07fbe4337
HaByeong <jhschris8080@naver.com>
2023-01-11 16:42:05 +0900

    feat: Make main and make score system

--- numguess.py
+++ numguess.py
@@ -16,9 +16,24 @@ def compare_line(guess,answer):
       print('*****************')
       sleep(1)
     print(f"You got it right!!!! The answer is {answer}!!")
+    return "win"
   elif(answer>guess):
     print(f"That's too small..{user_name}")
     print(f"You lose~! So sad...answer is~~{answer}!")
+    return "lose"
   elif(answer<guess):
     print(f"That's too big..{user_name}")
-    print(f"You lose~! So sad...answer is~~{answer}!") 
+    print(f"You lose~! So sad...answer is~~{answer}!")
+    return "lose"
+score=0
+#Use function
+answer,user_name,guess=start_line()
+result=compare_line(guess,answer)
+#Score system
+if(result=="win"):
+    score+=10000
+elif(result=="lose"):
+    score-=50
+print(f"Your score is {score}!")
+
+
